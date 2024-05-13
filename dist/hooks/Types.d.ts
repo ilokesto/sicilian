497c40e47937327ce317562659b97d86fdcb6e4a
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-05-14 01:42:21 +0900

    build : build

--- dist/hooks/Types.d.ts
+++ dist/hooks/Types.d.ts
@@ -32,21 +32,22 @@ export type RegisterErrorObj = {
     RegExp?: RegExpErrorObj | Array<RegExpErrorObj>;
     customChecker?: CustomCheckerErrorObj | Array<CustomCheckerErrorObj>;
 };
-export type Register<K> = (name: K, ErrorObj?: RegisterErrorObj) => {
-    value: string;
-    name: K;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    onBlur: (e: ChangeEvent<HTMLInputElement>) => void;
-    onFocus: (e: ChangeEvent<HTMLInputElement>) => void;
-};
-export type UseRegister = <T extends InitState>(From: Context<Store<T>>, Error: Context<Store<T>>) => Register<keyof T>;
-export type UseContextState = <T extends InitState>(context: Context<Store<T>>) => T;
 type OnBlurProps = {
     ErrorObj?: RegisterErrorObj;
     value: string;
     setError: (action: any) => void;
 };
-export type RegistOnBlur = (onblurProps: OnBlurProps) => (e: ChangeEvent<HTMLInputElement>) => void;
+export type RegistOnBlur = (onBlurProps: OnBlurProps) => (e: ChangeEvent<HTMLInputElement>) => void;
 export type RegistOnChange = <T extends InitState>(setStore: (action: T) => void) => (e: ChangeEvent<HTMLInputElement>, customValue?: string) => void;
 export type RegistOnSubmit = <T extends InitState>(FormState: () => T, ErrorState: () => T) => (fn: (data: InitState) => void) => (e: FormEvent) => void;
+export type RegistOnFocus = (e: ChangeEvent<HTMLInputElement>) => void;
+export type Register<K> = (name: K, ErrorObj?: RegisterErrorObj) => {
+    value: string;
+    name: K;
+    onChange: ReturnType<RegistOnChange>;
+    onBlur: ReturnType<RegistOnBlur>;
+    onFocus: RegistOnFocus;
+};
+export type UseRegister = <T extends InitState>(From: Context<Store<T>>, Error: Context<Store<T>>) => Register<keyof T>;
+export type UseContextState = <T extends InitState>(context: Context<Store<T>>) => T;
 export {};
