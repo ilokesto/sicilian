6a3e30987ecc11e1ebbdeac59cab8b6b393056ca
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-12-25 01:19:44 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
+import visualizer from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ filename: './stats.html', open: true })],
   build: {
     minify: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'sicilian',
       fileName: (format) => `sicilian.${format}.js`,
-      formats: ['es', 'umd'], // ES 모듈과 UMD 번들 생성
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
