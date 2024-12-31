041611c99670654043ab4129e2d3ca09b027e481
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-12-31 15:12:24 +0900

    안되나봐

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react(), viteCompression() ],
+  plugins: [react()],
   build: {
     minify: true,
     lib: {
