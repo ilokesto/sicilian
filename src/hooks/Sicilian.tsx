997f492c04e03d2affd626a08a526d5e338b30c3
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-01-02 19:43:42 +0900

    feat : umd 제공

--- src/hooks/Sicilian.tsx
+++ src/hooks/Sicilian.tsx
@@ -6,8 +6,6 @@ import { init } from "./funcs/init";
 // 함수 시그니처 오버로딩
 export function Sicilian<T extends InitState>(optionWithInitValue: SicilianProps<T>): SicilianReturnType<T>
 export function Sicilian<T extends InitState>(initValue: T, option?: Omit<SicilianProps<T>, "initValue">): SicilianReturnType<T>
-
-// 실제 구현
 export function Sicilian<T extends InitState>(
   initValueOrOptions: T | SicilianProps<T>,
   options?: Omit<SicilianProps<T>, "initValue">
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression() ],
   build: {
     minify: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'sicilian',
       fileName: (format) => `sicilian.${format}.js`,
-      formats: ['es'],
+      formats: ['es', "umd"],
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
