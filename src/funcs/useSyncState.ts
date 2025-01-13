cbb0154d15e1f2bd10c4b59f2100315294a3238f
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-01-13 19:17:37 +0900

    feat: useSyncState에서 store 매개변수를 구조 분해 할당으로 변경하여 가독성 향상

--- src/funcs/useSyncState.ts
+++ src/funcs/useSyncState.ts
@@ -1,8 +1,8 @@
 import { useSyncExternalStore } from "react";
 import type { ExtractKeys, InitState, Store } from "../types";
 
-export function useSyncState <T extends InitState>(store:Store<T>, name?: ExtractKeys<T>) {
-  const { getStore, subscribe } = store;
+export function useSyncState <T extends InitState>({ getStore, subscribe }: Store<T>, name?: ExtractKeys<T>) {
   const notationSnapshot = () => (name ? getStore()[name] : getStore());
+
   return useSyncExternalStore(subscribe, notationSnapshot, notationSnapshot);
 };
